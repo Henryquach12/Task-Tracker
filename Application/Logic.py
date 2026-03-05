f92ee905544e6cf23529d9c413f99aa59b9cd985
Henry Quach <haoquach1209@gmail.con>
2026-03-05 17:08:58 +1030

    Final commit

--- Application/Logic.py
+++ Application/Logic.py
@@ -1,12 +1,14 @@
 import Database
+from datetime import date
 
 class Operation:
     def __init__(self):
         self.database = Database.Json()
+        self.time = date.today().isoformat()
 
     def add_task(self, description, task=None):
         id_counter = self.database.generate_id()
-        data = {"id" : id_counter, "task" : description, "status" : None}
+        data = {"id" : id_counter, "task" : description, "status" : 'todo', "createdAt": self.time, "updateAt": self.time}
         self.database.tasks.append(data)
         self.database.save_task()
         print(f"Task added successfully (ID:{id_counter})")
@@ -15,15 +17,49 @@ class Operation:
         for task in self.database.tasks:
             if task["id"] == ID:
                 task["task"] = description
+                task["updateAt"] = self.time
+                print(f"Task updated successfully (ID:{ID})")
                 return self.database.save_task()
         print("No record was found! Check ID again...")
 
     def delete_task(self, ID):
+        valid_id = any(task["id"] == ID for task in self.database.tasks)
+        if not valid_id:
+            print("ID of task does not exist!")
+            return
         self.database.tasks = [task for task in self.database.tasks if task["id"] != ID]
         for task in self.database.tasks:
             if task["id"] > ID:
                 task["id"] -= 1
+        print(f"Task deleted successfully (ID:{ID})")
         self.database.save_task()
+
+    def mark_task(self, ID, status):
+        valid_id = any(task["id"] == ID for task in self.database.tasks)
+        if not valid_id:
+            print("ID of task does not exist!")
+            return
+        for task in self.database.tasks:
+            if task["id"] == ID:
+                if status == "mark-in-progress":
+                    task["status"] = "in-progress"
+                else:
+                    task["status"] = "done"
+        print(f"Task marked successfully (ID:{ID})")
+        return self.database.save_task()
+    
+    def list_task(self, status=None):
+        print(f"{'ID':<3} {'Task':<20} {'Status':<12}")
+        print("-"*35)
+        if status == None:
+            for task in self.database.tasks:
+                print(f"{task['id']:<3} {task['task']:<20} {task['status']:<12}")
+        else:
+            for task in self.database.tasks:
+                if task['status'] == status:
+                    print(f"{task['id']:<3} {task['task']:<20} {task['status']:<12}")
+
+    
                 
 
 
--- Application/Main.py
+++ Application/Main.py
@@ -10,6 +10,12 @@ def call_update(args, logic):
 def call_delete(args, logic):
     logic.delete_task(ID=args.ID)
 
+def call_mark(args, logic):
+    logic.mark_task(ID=args.ID, status=args.command)
+
+def call_list(args, logic):
+    logic.list_task(status=args.status)
+
 def main():
     # Main parser
     parser = argparse.ArgumentParser(
@@ -38,15 +44,18 @@ def main():
     # 'Marking' command 
     progress_parser = subparsers.add_parser('mark-in-progress')
     progress_parser.add_argument('ID', type=int)
+    progress_parser.set_defaults(func=call_mark)
 
     done_parser = subparsers.add_parser('mark-done')
     done_parser.add_argument('ID', type=int)
+    done_parser.set_defaults(func=call_mark)
 
     # 'List' command
     list_parser = subparsers.add_parser('list')
     list_parser.add_argument('status', nargs='?', 
                         choices=['todo', 'in-progress', 'done']
                         )
+    list_parser.set_defaults(func=call_list)
     
     logic = Logic.Operation()
     args = parser.parse_args()
@@ -55,5 +64,6 @@ def main():
         args.func(args, logic)
     else:
         parser.print_help()
+
 if __name__ == "__main__":
     main()
